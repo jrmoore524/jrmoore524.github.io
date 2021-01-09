1d18ee611a2aed342f739af4b8cb79673416da1e
jrmoore524 <54375114+jrmoore524@users.noreply.github.com>
2021-01-08 19:53:16 -0600

    timing update

--- css/app3.js
+++ css/app3.js
@@ -1,6 +1,6 @@
 const tl = gsap.timeline({ defaults: { ease: "power1.out" } });
 
-tl.to(".text", { y: "0%", duration: .65, stagger: 0.50 });
+tl.to(".text", { y: "0%", duration: .4, stagger: 0.50 });
 tl.to(".slider", { y: "-100%", duration: 1.5, delay: 0.5 });
 tl.to(".intro", { y: "-100%", duration: 1 }, "-=1");
 tl.fromTo("nav", { opacity: 0 }, { opacity: 1, duration: 1 });
